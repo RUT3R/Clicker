1d514cec150a3bf483ab26838fbf33e59617ee5f
RUTER <rutkowskimichal03@gmail.com>
2022-03-31 16:16:34 +0200

    1/2 ładzia

--- Clicker/Form1.Designer.cs
+++ Clicker/Form1.Designer.cs
@@ -28,11 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // button1
@@ -43,7 +50,7 @@
             this.button1.Cursor = System.Windows.Forms.Cursors.Cross;
             this.button1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.button1.ForeColor = System.Drawing.Color.Black;
-            this.button1.Location = new System.Drawing.Point(321, 125);
+            this.button1.Location = new System.Drawing.Point(62, 117);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(200, 200);
             this.button1.TabIndex = 0;
@@ -55,48 +62,110 @@
             // 
             this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(638, 9);
+            this.label1.Location = new System.Drawing.Point(87, 53);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(150, 50);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Mamona:$0";
+            this.label1.Text = "SUBY:0";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // button2
             // 
+            this.button2.BackColor = System.Drawing.Color.Crimson;
             this.button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button2.Location = new System.Drawing.Point(694, 389);
+            this.button2.Location = new System.Drawing.Point(331, 268);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(94, 49);
+            this.button2.Size = new System.Drawing.Size(102, 60);
             this.button2.TabIndex = 2;
-            this.button2.Text = "UPGRADE";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Text = "UPGRADE\r\n(10$)";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label2
             // 
             this.label2.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(694, 314);
+            this.label2.Location = new System.Drawing.Point(331, 117);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(94, 49);
             this.label2.TabIndex = 3;
             this.label2.Text = "lvl: 0";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(521, 268);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(111, 23);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "0";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(521, 102);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(111, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Zwiększ szybkość";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(521, 210);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(111, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Zwięsz ilość";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Red;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(521, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 29);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Auto upgrade";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(521, 156);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(111, 23);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.Text = "0";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Tick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Olive;
+            this.ClientSize = new System.Drawing.Size(701, 393);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.MinimumSize = new System.Drawing.Size(200, 200);
             this.Name = "Form1";
             this.RightToLeftLayout = true;
-            this.Text = "Form1";
+            this.Text = "EDWIN SYMULATOR";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -106,5 +175,11 @@
         private Label label1;
         private Button button2;
         private Label label2;
+        private TextBox textBox1;
+        private Button button3;
+        private Button button4;
+        private Label label3;
+        private TextBox textBox2;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- Clicker/Form1.cs
+++ Clicker/Form1.cs
@@ -4,17 +4,23 @@ namespace Clicker
     {
         int buttonlvl;
         int cash;
+        int speed;
+        int quantity;
         public Form1()
         {
             InitializeComponent();
             cash = 0;
-            buttonlvl = 0;
+            buttonlvl = 1;
+            speed = 10;
+            quantity = 1;
+            textBox1.Text = speed.ToString();
+            textBox2.Text = quantity.ToString();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             cash += (int)Math.Pow(10, buttonlvl-1);
-            label1.Text = "Mamona:$" + cash.ToString();
+            label1.Text = "SUBY:" + cash.ToString();
 
 
         }
@@ -23,19 +29,60 @@ namespace Clicker
         {
 
             int upgradeCost = (int)Math.Pow(10, buttonlvl);
-            if(cash >= upgradeCost)
+            if (cash >= upgradeCost)
             {
 
                 buttonlvl++;
-                button1.Text = "lvl:" + buttonlvl.ToString();
+                label2.Text = "lvl:" + buttonlvl.ToString();
                 cash -= upgradeCost;
-                label1.Text = "Mamona:$" + cash.ToString();
+                //label1.Text = "Mamona:$" + cash.ToString();
                 string nextUpgradeCost = "($" + Math.Pow(10, buttonlvl).ToString() + ")";
                 button2.Text = "Upgrade\n" + nextUpgradeCost;
 
             }
 
-            
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            quantity++;
+            textBox1.Text = quantity.ToString();
+            timer1.Interval = (60 / quantity) * 1000;
+            if (!timer1.Enabled)
+                timer1.Enabled = true;
+        }
+
+        private void Tick(object sender, EventArgs e)
+        {
+            cash += speed;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            speed += 10;
+            textBox2.Text = speed.ToString();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            speed += 10;
+            textBox2.Text = speed.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int upgradeCost = quantity * 100;
+            if(cash >= upgradeCost) {
+                quantity++;
+                textBox1.Text = quantity.ToString();
+                timer1.Interval = (60 / quantity) * 1000;
+                if (!timer1.Enabled)
+                    timer1.Enabled = true;
+                cash -= upgradeCost;
+                label1.text = "SUBY:" + cash.ToString();
+            }
 
         }
     }
